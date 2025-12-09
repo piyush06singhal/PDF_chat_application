921e1f36cb2241c1b57059e65c2eeedc12a373d5
Piyush Singhal <164621184+piyush06singhal@users.noreply.github.com>
2025-12-09 22:36:42 +0530

    Refactor app.py for improved functionality and UI

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 import os
